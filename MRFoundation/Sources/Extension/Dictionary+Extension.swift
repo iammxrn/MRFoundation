0c321646be19d004e147e2b382faabeddd0ac4e7
Roman Mogutnov <mixroman@yahoo.com>
2023-01-27 22:02:30 +0400

    Documentation, Tests and New Data Structure - Queue

--- MRFoundation/Sources/Extension/Dictionary+Extension.swift
+++ MRFoundation/Sources/Extension/Dictionary+Extension.swift
@@ -24,17 +24,37 @@ extension Dictionary {
 
 extension Dictionary {
 
+    /// Same values, corresponding to `map`ped keys.
+      ///
+      /// - Parameter transform: Accepts each key of the dictionary as its parameter
+      ///   and returns a key for the new dictionary.
+      /// - Postcondition: The collection of transformed keys must not contain duplicates.
     public func mapKeys<Transformed>(_ transform: (Key) throws -> Transformed) rethrows -> [Transformed: Value] {
         .init(uniqueKeysWithValues: try map { (try transform($0.key), $0.value) })
     }
-    
-    public func mapKeys<Transformed>(_ transform: (Key) throws -> Transformed,
-                                     uniquingKeysWith combine: (Value, Value) throws -> Value) rethrows -> [Transformed: Value] {
+
+    /// Same values, corresponding to `map`ped keys.
+      ///
+      /// - Parameters:
+      ///   - transform: Accepts each key of the dictionary as its parameter
+      ///     and returns a key for the new dictionary.
+      ///   - combine: A closure that is called with the values for any duplicate
+      ///     keys that are encountered. The closure returns the desired value for
+      ///     the final dictionary.
+    public func mapKeys<Transformed>(
+        _ transform: (Key) throws -> Transformed,
+        uniquingKeysWith combine: (Value, Value) throws -> Value
+    ) rethrows -> [Transformed: Value] {
         try .init(map { (try transform($0.key), $0.value) }, uniquingKeysWith: combine)
     }
     
-    
+    /// `compactMap`ped keys, with their values.
+    ///
+    /// - Parameter transform: Accepts each key of the dictionary as its parameter
+    ///   and returns a potential key for the new dictionary.
+    /// - Postcondition: The collection of transformed keys must not contain duplicates.
     public func compactMapKeys<Transformed>(_ transform: (Key) throws -> Transformed?) rethrows -> [Transformed: Value] {
         .init(uniqueKeysWithValues: try compactMap { (try transform($0.key), $0.value) as? (Transformed, Value) })
     }
+    
 }
--- MRFoundation/Sources/UserDefaultsProvider/MRUserDefaultsProvider.swift
+++ MRFoundation/Sources/UserDefaultsProvider/MRUserDefaultsProvider.swift
@@ -8,40 +8,65 @@
 
 import Foundation
 
-
+/// MRUserDefaultsProviderKey
+///
+/// A protocol that defines a key type that can be used with MRUserDefaultsProvider.
+/// ```swift
+/// enum MyUserDefaultsProviderKey: String, MRUserDefaultsProviderKey {
+///    case myProperty
+///    case anotherProperty
+/// }
+/// ```
 public protocol MRUserDefaultsProviderKey: RawRepresentable where RawValue == String {}
 
+/// MRUserDefaultsProvider
+///
+/// A class that provides a simple interface for interacting with UserDefaults.
 open class MRUserDefaultsProvider<Key: MRUserDefaultsProviderKey> {
-    
-    
+
     // MARK: - Private Properties
-    
+
     private let userDefaults: UserDefaults
-    
-    
-    // MARK: - Init
-    
+
+    // MARK: - Pubci Init
+
+    /// Initializes a new MRUserDefaultsProvider instance.
+    ///
+    /// - Parameters:
+    ///   - userDefaults: The UserDefaults instance to use for persistence.
     public init(userDefaults: UserDefaults) {
         self.userDefaults = userDefaults
     }
-    
-    
+
     // MARK: - Public Methods
-    
+
+    /// Persists data for a given key.
+    ///
+    /// - Parameters:
+    ///   - data: The data to persist.
+    ///   - key: The key to associate the data with.
     public func persist(_ data: Any, for key: Key) {
         userDefaults.set(data, forKey: key.rawValue)
         userDefaults.synchronize()
     }
-    
+
+    /// Erases data for a given key.
+    ///
+    /// - Parameters:
+    ///   - key: The key associated with the data to erase.
     public func eraseData(for key: Key) {
         userDefaults.removeObject(forKey: key.rawValue)
         userDefaults.synchronize()
     }
-    
+
+    /// Fetches data for a given key.
+    ///
+    /// - Parameters:
+    ///   - key: The key associated with the data to fetch.
+    /// - Returns: The fetched data, or nil if no data is associated with the key.
     public func fetchData<T>(for key: Key) -> T? {
         guard let object = userDefaults.object(forKey: key.rawValue) else { return nil }
-        
+
         return object as? T
     }
 }
-
