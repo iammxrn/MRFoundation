82d950f3cdfe16859c062a7ea87a73183decd53f
Vadzim Karonchyk <42875321+vadzimkaronchyk@users.noreply.github.com>
2023-01-30 19:13:09 +0300

    Codable support in Keychain

--- MRFoundation/Sources/KeychainProvider/MRKeychainProvider.swift
+++ MRFoundation/Sources/KeychainProvider/MRKeychainProvider.swift
@@ -30,14 +30,28 @@ open class MRKeychainProvider<Key: MRKeychainProviderKey> {
     // MARK: - Public Methods
     
     public func setValue(_ value: String, for key: Key) throws {
-        guard let encodedPassword = value.data(using: .utf8) else {
+        guard let encodedValue = value.data(using: .utf8) else {
             throw MRKeychainProviderError(
                 code: .string2DataConversion,
                 localizedDescription: "Invalid value: \(value)",
                 systemMessage: "Cannot convert value: \(value) to binary data"
             )
         }
-        
+        try setValue(encodedValue, for: key)
+    }
+    
+    public func setValue(_ value: Codable, for key: Key) throws {
+        guard let encodedValue = try? JSONEncoder().encode(value) else {
+            throw MRKeychainProviderError(
+                code: .data2CodableConversion,
+                localizedDescription: "Invalid value: \(value)",
+                systemMessage: "Cannot convert value: \(value) to binary data"
+            )
+        }
+        try setValue(encodedValue, for: key)
+    }
+    
+    public func setValue(_ value: Data, for key: Key) throws {
         var query = keychainQueryable.query
         query[String(kSecAttrAccount)] = key.rawValue
         
@@ -45,7 +59,7 @@ open class MRKeychainProvider<Key: MRKeychainProviderKey> {
         switch status {
         case errSecSuccess:
             var attributesToUpdate: [String: Any] = [:]
-            attributesToUpdate[String(kSecValueData)] = encodedPassword
+            attributesToUpdate[String(kSecValueData)] = value
             
             status = SecItemUpdate(query as CFDictionary,
                                    attributesToUpdate as CFDictionary)
@@ -54,7 +68,7 @@ open class MRKeychainProvider<Key: MRKeychainProviderKey> {
             }
             
         case errSecItemNotFound:
-            query[String(kSecValueData)] = encodedPassword
+            query[String(kSecValueData)] = value
             
             status = SecItemAdd(query as CFDictionary, nil)
             if status != errSecSuccess {
@@ -67,6 +81,37 @@ open class MRKeychainProvider<Key: MRKeychainProviderKey> {
     }
     
     public func getValue(for key: Key) throws -> String? {
+        guard let encodedValue: Data = try getValue(for: key) else {
+            return nil
+        }
+        
+        guard let value = String(data: encodedValue, encoding: .utf8) else {
+            throw MRKeychainProviderError(
+                code: .data2StringConversion,
+                localizedDescription: "Data corrupted",
+                systemMessage: "Cannot convert binary data to string"
+            )
+        }
+        
+        return value
+    }
+    
+    public func getValue<T: Codable>(for key: Key) throws -> T? {
+        let encodedValue: Data? = try getValue(for: key)
+        do {
+            return try encodedValue.map {
+                try JSONDecoder().decode(T.self, from: $0)
+            }
+        } catch {
+            throw MRKeychainProviderError(
+                code: .data2CodableConversion,
+                localizedDescription: "Data corrupted",
+                systemMessage: "Cannot convert binary data to codable"
+            )
+        }
+    }
+    
+    public func getValue(for key: Key) throws -> Data? {
         var query = keychainQueryable.query
         query[String(kSecMatchLimit)] = kSecMatchLimitOne
         query[String(kSecReturnAttributes)] = kCFBooleanTrue
@@ -82,16 +127,15 @@ open class MRKeychainProvider<Key: MRKeychainProviderKey> {
         case errSecSuccess:
             guard
                 let queriedItem = queryResult as? [String: Any],
-                let passwordData = queriedItem[String(kSecValueData)] as? Data,
-                let password = String(data: passwordData, encoding: .utf8)
+                let passwordData = queriedItem[String(kSecValueData)] as? Data
             else {
                 throw MRKeychainProviderError(
-                    code: .data2StringConversion,
+                    code: .dataRetrieving,
                     localizedDescription: "Data corrupted",
-                    systemMessage: "Cannot convert binary data to string"
+                    systemMessage: "Cannot retreive binary data"
                 )
             }
-            return password
+            return passwordData
             
         case errSecItemNotFound:
             return nil
--- MRFoundation/Sources/KeychainProvider/MRKeychainProviderError.swift
+++ MRFoundation/Sources/KeychainProvider/MRKeychainProviderError.swift
@@ -13,6 +13,9 @@ public enum MRKeychainProviderErrorCode: Int, MRErrorCode {
     case unknown = 0
     case string2DataConversion = 1
     case data2StringConversion = 2
+    case dataRetrieving = 3
+    case codable2DataConversion = 4
+    case data2CodableConversion = 5
 }
 
 public final class MRKeychainProviderError: MRError<MRKeychainProviderErrorCode> {
